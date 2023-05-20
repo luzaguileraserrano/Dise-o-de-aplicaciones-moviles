b2e7c81d1f3b153c8568c38c84570022282c583f
luzaguileraserrano <80918257+luzaguileraserrano@users.noreply.github.com>
2023-05-20 03:37:35 -0600

    Add files via upload
    Hola Maestra, le envío el código my las imagenes del proyecto final, muchas gracias

--- main.dart
+++ main.dart
@@ -1,133 +1,93 @@
-import 'package:demo/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-    );
-    
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+import 'package:flutter/material.dart';  
+  
+void main() { runApp(MyApp()); }  
+class MyApp extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: "Catálogo de películas",
+      theme: ThemeData(primarySwatch: Colors.lightBlue), 
+        home: Scaffold(
+          drawer:Drawer(
+            child:Container(
+              color: Colors.green,
+              child: Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(top:50, bottom:20),
+                    child: Image.asset("assets/2.jpg"),
+                  ),
+                  const Text ("El hombre araña", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top:50, bottom: 20),
+                    padding: const EdgeInsets.all(20),
+                    width:double.infinity,
+                    color: Colors.grey[50],
+                    child: const Text("Año:1999"),
+                  ),
+                   Container(
+                    margin: const EdgeInsets.only(top: 2),
+                    padding: const EdgeInsets.all(20),
+                    width:double.infinity,
+                    color: Colors.grey[50],
+                    child: const Text("Director: Marvel"),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 2),
+                    padding: const EdgeInsets.all(20),
+                    width:double.infinity,
+                    color: Colors.grey[50],
+                    child: const Text("Género:Acción"),
+                  ),
+                   Container(
+                    margin: const EdgeInsets.only(top: 2),
+                    padding: const EdgeInsets.all(20),
+                    width:double.infinity,
+                    color: Colors.grey[50],
+                    child: const Text("Sinopsis: excelente película"),
+                  ),
+                  Expanded(child: Container()),
+                   Container(
+                    margin: const EdgeInsets.only(top: 2),
+                    padding: const EdgeInsets.all(20),
+                    width:double.infinity,
+                    color: Colors.black87,
+                    alignment: Alignment.center,
+                    child: const Text("Sign Out", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+    
+                  )
+                ],
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          appBar: AppBar(
+            title: Text('Catálogo de películas'),
+          ),
+          body: Center(
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Image.asset("assets/1.jpeg"),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    child: Text(
+                    "Bienvenido a mi App",
+                    style: TextStyle(
+                      color:Colors.amber,
+                      fontSize: 26
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+          ),
+        
+        ),  
+    );  
+  }  
+}  
+  
